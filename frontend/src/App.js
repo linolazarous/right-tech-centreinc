babd5452e8bb5007dc32e1ab40c0217e063d5a5a
linolazarous <linolaku560@gmail.com>
2025-06-15 18:50:37 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,25 +7,53 @@ import LiveClass from './pages/LiveClass';
 import Forum from './pages/Forum';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
+import ContactPage from './pages/ContactPage'; // New contact page
+import FormSuccess from './pages/FormSuccess'; // New success page
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { Toaster } from 'react-hot-toast'; // For notification messages
 
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/courses" element={<Courses />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/live-class" element={<LiveClass />} />
-                <Route path="/forum" element={<Forum />} />
-                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                <Route path="/terms-of-service" element={<TermsOfService />} />
-            </Routes>
-            <Footer />
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        
+        {/* Notification system */}
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            duration: 5000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+          }}
+        />
+
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/live-class" element={<LiveClass />} />
+            <Route path="/forum" element={<Forum />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
+            
+            {/* New routes for contact form flow */}
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/form-success" element={<FormSuccess />} />
+            
+            {/* 404 Page - Add this if you want */}
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
