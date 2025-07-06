dc378a30eedf763cb13df23b47f2a2aa2f699a28
linolazarous <linolaku560@gmail.com>
2025-07-06 18:54:17 +0200

    Update jobRoutes.js

--- backend/src/routes/jobRoutes.js
+++ backend/src/routes/jobRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const jobController = require('../controllers/jobController');
 const router = express.Router();
+const jobController = require('../controllers/jobController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateJobQuery } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.get('/jobs/recommendations', jobController.getJobRecommendations);
+router.get(
+  '/jobs/recommendations',
+  authMiddleware,
+  validateJobQuery,
+  rateLimit('50req/hour'),
+  jobController.getJobRecommendations
+);
 
 module.exports = router;
