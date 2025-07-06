804d61370d2da232943c687531c0dd341e01b8db
linolazarous <linolaku560@gmail.com>
2025-07-06 19:22:35 +0200

    Update affliationService.js

--- backend/src/services/affliationService.js
+++ backend/src/services/affliationService.js
@@ -1,13 +1,41 @@
 const AffiliationModel = require('../models/affiliationModel');
+const logger = require('../utils/logger');
 
 class AffiliationService {
+  /**
+   * Get all affiliations
+   * @returns {Promise<Array>} List of affiliations
+   */
   static async getAffiliations() {
-    return await AffiliationModel.find();
+    try {
+      logger.info('Fetching all affiliations');
+      return await AffiliationModel.find().sort({ name: 1 });
+    } catch (error) {
+      logger.error(`Error fetching affiliations: ${error.message}`);
+      throw error;
+    }
   }
 
+  /**
+   * Add a new affiliation
+   * @param {Object} affiliationData - Affiliation data
+   * @returns {Promise<Object>} Created affiliation
+   */
   static async addAffiliation(affiliationData) {
-    const newAffiliation = new AffiliationModel(affiliationData);
-    return await newAffiliation.save();
+    try {
+      logger.info('Creating new affiliation');
+      const newAffiliation = new AffiliationModel(affiliationData);
+      await newAffiliation.validate();
+      await newAffiliation.save();
+      logger.info(`Affiliation created: ${newAffiliation._id}`);
+      return newAffiliation;
+    } catch (error) {
+      logger.error(`Error creating affiliation: ${error.message}`);
+      if (error.code === 11000) {
+        throw new Error('Affiliation with this name already exists');
+      }
+      throw error;
+    }
   }
 }
 
