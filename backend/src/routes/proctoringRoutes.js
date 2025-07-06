e6424a0b472e52b16e7e78eaf59a57b76faee4a2
linolazarous <linolaku560@gmail.com>
2025-07-06 19:05:00 +0200

    Update proctoringRoutes.js

--- backend/src/routes/proctoringRoutes.js
+++ backend/src/routes/proctoringRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const proctoringController = require('../controllers/proctoringController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateProctoring } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post('/monitor-exam', proctoringController.monitorExam);
+router.post(
+  '/monitor-exam',
+  authMiddleware,
+  validateProctoring,
+  rateLimit('10req/hour'),
+  proctoringController.monitorExam
+);
 
 module.exports = router;
