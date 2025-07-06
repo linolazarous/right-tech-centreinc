7e7c1912a2507fe4f6e7b61439b8e8127247b5c5
linolazarous <linolaku560@gmail.com>
2025-07-06 18:42:28 +0200

    Update arLearningRoute.js

--- backend/src/routes/arLearningRoute.js
+++ backend/src/routes/arLearningRoute.js
@@ -1,9 +1,18 @@
-const express = require("express");
-const { createLesson } = require("../controllers/arLearningController");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const express = require('express');
 const router = express.Router();
+const { createLesson } = require('../controllers/arLearningController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateARContent } = require('../middleware/validationMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post("/create", authMiddleware, createLesson);
+router.post(
+  '/create',
+  authMiddleware,
+  roleMiddleware(['instructor', 'admin']),
+  validateARContent,
+  rateLimit('20req/day'),
+  createLesson
+);
 
 module.exports = router;
