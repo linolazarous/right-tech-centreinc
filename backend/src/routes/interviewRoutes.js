4200d413c9bd4f69892718d2bbc1b0e68a22e204
linolazarous <linolaku560@gmail.com>
2025-07-06 18:52:31 +0200

    Update interviewRoutes.js

--- backend/src/routes/interviewRoutes.js
+++ backend/src/routes/interviewRoutes.js
@@ -1,10 +1,49 @@
-const express = require("express");
-const { getInterviewQuestions, conductMockInterview } = require("../controllers/interviewController");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const express = require('express');
 const router = express.Router();
+const { 
+  getJobMatches, 
+  createResume, 
+  getInterviewQuestions, 
+  conductMockInterview 
+} = require('../controllers/careerController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { 
+  validateJobMatchRequest,
+  validateResumeRequest,
+  validateInterviewRequest
+} = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post("/questions", authMiddleware, getInterviewQuestions);
-router.post("/mock-interview", authMiddleware, conductMockInterview);
+router.get(
+  '/job-matches',
+  authMiddleware,
+  validateJobMatchRequest,
+  rateLimit('30req/hour'),
+  getJobMatches
+);
+
+router.post(
+  '/create-resume',
+  authMiddleware,
+  validateResumeRequest,
+  rateLimit('10req/day'),
+  createResume
+);
+
+router.post(
+  '/interview-questions',
+  authMiddleware,
+  validateInterviewRequest,
+  rateLimit('20req/hour'),
+  getInterviewQuestions
+);
+
+router.post(
+  '/mock-interview',
+  authMiddleware,
+  validateInterviewRequest,
+  rateLimit('10req/hour'),
+  conductMockInterview
+);
 
 module.exports = router;
