e85823ab526a3c8e82edfc52ef63b66a643851ef
linolazarous <linolaku560@gmail.com>
2025-07-06 19:15:50 +0200

    Update vrLabRoutes.js

--- backend/src/routes/vrLabRoutes.js
+++ backend/src/routes/vrLabRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express');
-  const router = express.Router();
-  const vrLabController = require('../controllers/vrLabController');
+const router = express.Router();
+const vrLabController = require('../controllers/vrLabController');
+const { validateVRLabQuery } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-  router.get('/labs', vrLabController.getVRLabs);
+router.get(
+  '/labs',
+  validateVRLabQuery,
+  rateLimit('1000req/hour'),
+  vrLabController.getVRLabs
+);
 
-  module.exports = router;
+module.exports = router;
