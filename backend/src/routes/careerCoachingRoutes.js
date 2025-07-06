bdd45160be5ac087169c212dcf86fcf59610b046
linolazarous <linolaku560@gmail.com>
2025-07-06 18:45:56 +0200

    Update careerCoachingRoutes.js

--- backend/src/routes/careerCoachingRoutes.js
+++ backend/src/routes/careerCoachingRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
-  const router = express.Router();
-  const careerCoachingController = require('../controllers/careerCoachingController');
+const router = express.Router();
+const careerCoachingController = require('../controllers/careerCoachingController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateUserIdParam } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-  router.get('/advice/:userId', careerCoachingController.getCareerAdvice);
+router.get(
+  '/advice/:userId',
+  authMiddleware,
+  validateUserIdParam,
+  rateLimit('50req/hour'),
+  careerCoachingController.getCareerAdvice
+);
 
-  module.exports = router;
+module.exports = router;
