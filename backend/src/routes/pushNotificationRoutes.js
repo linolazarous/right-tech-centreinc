2e5b159a699ca84642a579820927f9ac265af553
linolazarous <linolaku560@gmail.com>
2025-07-06 19:05:41 +0200

    Update pushNotificationRoutes.js

--- backend/src/routes/pushNotificationRoutes.js
+++ backend/src/routes/pushNotificationRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const notificationController = require('../controllers/pushNotificationController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateNotification } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-// Send a push notification
-router.post('/send', notificationController.sendNotification);
+router.post(
+  '/send',
+  authMiddleware,
+  validateNotification,
+  rateLimit('50req/hour'),
+  notificationController.sendNotification
+);
 
 module.exports = router;
