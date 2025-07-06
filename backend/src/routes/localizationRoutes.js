4b15964048a25759d0019080994ed01c7513df98
linolazarous <linolaku560@gmail.com>
2025-07-06 18:58:02 +0200

    Update localizationRoutes.js

--- backend/src/routes/localizationRoutes.js
+++ backend/src/routes/localizationRoutes.js
@@ -1,9 +1,16 @@
-const express = require("express");
-const { translate } = require("../controllers/localizationController");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const express = require('express');
 const router = express.Router();
+const { translate } = require('../controllers/localizationController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateLocalization } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post("/translate", authMiddleware, translate);
+router.post(
+  '/translate',
+  authMiddleware,
+  validateLocalization,
+  rateLimit('100req/hour'),
+  translate
+);
 
 module.exports = router;
