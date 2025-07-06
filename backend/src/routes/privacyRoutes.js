4a8c6b417840551d63f559596e2b74ec29cdec2e
linolazarous <linolaku560@gmail.com>
2025-07-06 19:04:19 +0200

    Update privacyRoutes.js

--- backend/src/routes/privacyRoutes.js
+++ backend/src/routes/privacyRoutes.js
@@ -1,10 +1,22 @@
-const express = require("express");
-const { getPrivacyPolicy, deleteUserData } = require("../controllers/privacyController");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const express = require('express');
 const router = express.Router();
+const { getPrivacyPolicy, deleteUserData } = require('../controllers/privacyController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateDataDeletion } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.get("/privacy-policy", getPrivacyPolicy);
-router.delete("/delete-data", authMiddleware, deleteUserData);
+router.get(
+  '/privacy-policy',
+  rateLimit('1000req/hour'),
+  getPrivacyPolicy
+);
+
+router.delete(
+  '/delete-data',
+  authMiddleware,
+  validateDataDeletion,
+  rateLimit('1req/week'),
+  deleteUserData
+);
 
 module.exports = router;
