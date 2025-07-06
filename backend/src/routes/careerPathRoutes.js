e0fbebb859f08a7dc058db9441dd1f6151510774
linolazarous <linolaku560@gmail.com>
2025-07-06 18:46:36 +0200

    Update careerPathRoutes.js

--- backend/src/routes/careerPathRoutes.js
+++ backend/src/routes/careerPathRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
-  const router = express.Router();
-  const careerPathController = require('../controllers/careerPathController');
+const router = express.Router();
+const careerPathController = require('../controllers/careerPathController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateUserIdParam } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-  router.get('/recommend/:userId', careerPathController.recommendCareerPath);
+router.get(
+  '/recommend/:userId',
+  authMiddleware,
+  validateUserIdParam,
+  rateLimit('50req/hour'),
+  careerPathController.recommendCareerPath
+);
 
-  module.exports = router;
+module.exports = router;
