3fc8ee3ea363ab28b39164868540e80182732e61
linolazarous <linolaku560@gmail.com>
2025-07-06 19:07:49 +0200

    Update scholarshipRoutes.js

--- backend/src/routes/scholarshipRoutes.js
+++ backend/src/routes/scholarshipRoutes.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const scholarshipController = require('../controllers/scholarshipController');
 const router = express.Router();
+const scholarshipController = require('../controllers/scholarshipController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateScholarship } = require('../middleware/validationMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post('/scholarships/allocate', scholarshipController.allocateScholarship);
+router.post(
+  '/scholarships/allocate',
+  authMiddleware,
+  roleMiddleware(['admin', 'financial_aid']),
+  validateScholarship,
+  rateLimit('20req/day'),
+  scholarshipController.allocateScholarship
+);
 
 module.exports = router;
