14123d2490a3b5641312c6ace89c03463e24f96e
linolazarous <linolaku560@gmail.com>
2025-07-06 19:08:43 +0200

    Update skillsAssessmentRoutes.js

--- backend/src/routes/skillsAssessmentRoutes.js
+++ backend/src/routes/skillsAssessmentRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
-  const router = express.Router();
-  const skillAssessmentController = require('../controllers/skillAssessmentController');
+const router = express.Router();
+const skillAssessmentController = require('../controllers/skillAssessmentController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateSkillAssessment } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-  router.post('/assess', skillAssessmentController.assessSkill);
+router.post(
+  '/assess',
+  authMiddleware,
+  validateSkillAssessment,
+  rateLimit('30req/hour'),
+  skillAssessmentController.assessSkill
+);
 
-  module.exports = router;
+module.exports = router;
