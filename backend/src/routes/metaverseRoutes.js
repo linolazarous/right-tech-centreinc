2841a179cfc9d339069e1dcd0927cf1f01f3c755
linolazarous <linolaku560@gmail.com>
2025-07-06 18:58:57 +0200

    Update metaverseRoutes.js

--- backend/src/routes/metaverseRoutes.js
+++ backend/src/routes/metaverseRoutes.js
@@ -1,9 +1,18 @@
-const express = require("express");
-const { createCampus } = require("../controllers/metaverseController");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const express = require('express');
 const router = express.Router();
+const { createCampus } = require('../controllers/metaverseController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateMetaverseCampus } = require('../middleware/validationMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post("/create", authMiddleware, createCampus);
+router.post(
+  '/create',
+  authMiddleware,
+  roleMiddleware(['admin']),
+  validateMetaverseCampus,
+  rateLimit('5req/day'),
+  createCampus
+);
 
 module.exports = router;
