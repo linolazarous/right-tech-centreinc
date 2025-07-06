03cbc6bc2391277781c3e4fb89a6f88e7bee2a5e
linolazarous <linolaku560@gmail.com>
2025-07-06 19:07:08 +0200

    Update resumeBuilderRoutes.js

--- backend/src/routes/resumeBuilderRoutes.js
+++ backend/src/routes/resumeBuilderRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
-  const router = express.Router();
-  const resumeBuilderController = require('../controllers/resumeBuilderController');
+const router = express.Router();
+const resumeBuilderController = require('../controllers/resumeBuilderController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateUserIdParam } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-  router.get('/generate/:userId', resumeBuilderController.generateResume);
+router.get(
+  '/generate/:userId',
+  authMiddleware,
+  validateUserIdParam,
+  rateLimit('5req/day'),
+  resumeBuilderController.generateResume
+);
 
-  module.exports = router;
+module.exports = router;
