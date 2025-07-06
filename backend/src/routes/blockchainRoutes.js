4536302cf923e79fdc06db06d8fd12262890a52f
linolazarous <linolaku560@gmail.com>
2025-07-06 18:45:15 +0200

    Update blockchainRoutes.js

--- backend/src/routes/blockchainRoutes.js
+++ backend/src/routes/blockchainRoutes.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const blockchainController = require('../controllers/blockchainController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateCertificate } = require('../middleware/validationMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post('/issue-certificate', blockchainController.issueCertificate);
+router.post(
+  '/issue-certificate',
+  authMiddleware,
+  roleMiddleware(['admin', 'registrar']),
+  validateCertificate,
+  rateLimit('50req/day'),
+  blockchainController.issueCertificate
+);
 
 module.exports = router;
