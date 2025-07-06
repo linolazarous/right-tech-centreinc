1f3773f97c72ade12a21850728afadc9207e1b2f
linolazarous <linolaku560@gmail.com>
2025-07-06 18:47:52 +0200

    Update codingChallengeRoutes.js

--- backend/src/routes/codingChallengeRoutes.js
+++ backend/src/routes/codingChallengeRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express');
-  const router = express.Router();
-  const codingChallengeController = require('../controllers/codingChallengeController');
+const router = express.Router();
+const codingChallengeController = require('../controllers/codingChallengeController');
+const { validateChallengeQuery } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-  router.get('/challenges', codingChallengeController.getCodingChallenges);
+router.get(
+  '/challenges',
+  validateChallengeQuery,
+  rateLimit('1000req/hour'),
+  codingChallengeController.getCodingChallenges
+);
 
-  module.exports = router;
+module.exports = router;
