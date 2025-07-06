e49c8d4ad4e87da28182e365c16942dc40fc6e75
linolazarous <linolaku560@gmail.com>
2025-07-06 19:10:58 +0200

    Update subscriptionRoutes.js

--- backend/src/routes/subscriptionRoutes.js
+++ backend/src/routes/subscriptionRoutes.js
@@ -1,10 +1,23 @@
-const express = require("express");
-const { subscribe, getUserSubscriptions } = require("../controllers/subscriptionController");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const express = require('express');
 const router = express.Router();
+const { subscribe, getUserSubscriptions } = require('../controllers/subscriptionController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateSubscription } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post("/", authMiddleware, subscribe);
-router.get("/:userId", authMiddleware, getUserSubscriptions);
+router.post(
+  '/',
+  authMiddleware,
+  validateSubscription,
+  rateLimit('5req/day'),
+  subscribe
+);
+
+router.get(
+  '/:userId',
+  authMiddleware,
+  rateLimit('50req/hour'),
+  getUserSubscriptions
+);
 
 module.exports = router;
