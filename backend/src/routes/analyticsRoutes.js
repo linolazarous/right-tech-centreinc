ce8fc23d5723c752a83368dfb92b0c9b6c446908
linolazarous <linolaku560@gmail.com>
2025-07-06 18:41:52 +0200

    Update analyticsRoutes.js

--- backend/src/routes/analyticsRoutes.js
+++ backend/src/routes/analyticsRoutes.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const analyticsController = require('../controllers/analyticsController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateUserIdParam } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.get('/progress/:userId', analyticsController.getStudentProgress);
-router.get('/engagement/:userId', analyticsController.getEngagementMetrics);
+router.get(
+  '/progress/:userId',
+  authMiddleware,
+  validateUserIdParam,
+  rateLimit('100req/hour'),
+  analyticsController.getStudentProgress
+);
+
+router.get(
+  '/engagement/:userId',
+  authMiddleware,
+  validateUserIdParam,
+  rateLimit('100req/hour'),
+  analyticsController.getEngagementMetrics
+);
 
 module.exports = router;
