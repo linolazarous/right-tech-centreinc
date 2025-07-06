e233c8e62830bcc02191853490a2debaf7422697
linolazarous <linolaku560@gmail.com>
2025-07-06 18:55:07 +0200

    Update languageSwitcherRoutes.js

--- backend/src/routes/languageSwitcherRoutes.js
+++ backend/src/routes/languageSwitcherRoutes.js
@@ -1,9 +1,23 @@
-const express = require("express");
-const { translate } = require("../controllers/languageSwitcherController");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const express = require('express');
 const router = express.Router();
+const { translate } = require('../controllers/languageSwitcherController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateTranslation } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post("/translate", authMiddleware, translate);
+router.post(
+  '/translate',
+  authMiddleware,
+  validateTranslation,
+  rateLimit('100req/hour'),
+  translate
+);
+
+// Add endpoint to get supported languages
+router.get(
+  '/supported-languages',
+  rateLimit('1000req/hour'),
+  (req, res) => res.json({ supportedLanguages: ['en', 'es', 'fr', 'de'] })
+);
 
 module.exports = router;
