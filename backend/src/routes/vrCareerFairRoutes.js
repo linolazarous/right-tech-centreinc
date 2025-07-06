9a17b8186d69b8b6844e2382736d98efd3b60406
linolazarous <linolaku560@gmail.com>
2025-07-06 19:15:10 +0200

    Update vrCareerFairRoutes.js

--- backend/src/routes/vrCareerFairRoutes.js
+++ backend/src/routes/vrCareerFairRoutes.js
@@ -1,9 +1,18 @@
-const express = require("express");
-const { createEvent } = require("../controllers/vrCareerFairController");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const express = require('express');
 const router = express.Router();
+const { createEvent } = require('../controllers/vrCareerFairController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateVREvent } = require('../middleware/validationMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post("/create-event", authMiddleware, createEvent);
+router.post(
+  '/create-event',
+  authMiddleware,
+  roleMiddleware(['admin', 'career_services']),
+  validateVREvent,
+  rateLimit('5req/day'),
+  createEvent
+);
 
 module.exports = router;
