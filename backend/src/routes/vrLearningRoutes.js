afa06a4818f3e1d720844fb70d60afd36715b842
linolazarous <linolaku560@gmail.com>
2025-07-06 19:17:37 +0200

    Update vrLearningRoutes.js

--- backend/src/routes/vrLearningRoutes.js
+++ backend/src/routes/vrLearningRoutes.js
@@ -1,9 +1,18 @@
-const express = require("express");
-const { createLesson } = require("../controllers/vrLearningController");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const express = require('express');
 const router = express.Router();
+const { createLesson } = require('../controllers/vrLearningController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateVRLesson } = require('../middleware/validationMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post("/create", authMiddleware, createLesson);
+router.post(
+  '/create',
+  authMiddleware,
+  roleMiddleware(['instructor', 'admin']),
+  validateVRLesson,
+  rateLimit('10req/day'),
+  createLesson
+);
 
 module.exports = router;
