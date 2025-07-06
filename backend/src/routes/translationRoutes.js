feb8e50b9507cc8966507f3ceeef9f3a58c23d68
linolazarous <linolaku560@gmail.com>
2025-07-06 19:12:09 +0200

    Update translationRoutes.js

--- backend/src/routes/translationRoutes.js
+++ backend/src/routes/translationRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
-    const translationController = require('../controllers/translationController');
-    const router = express.Router();
+const router = express.Router();
+const translationController = require('../controllers/translationController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateTranslation } = require('../middleware/validationMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-    router.post('/translate', translationController.translateText);
+router.post(
+  '/translate',
+  authMiddleware,
+  validateTranslation,
+  rateLimit('100req/hour'),
+  translationController.translateText
+);
 
-    module.exports = router;
+module.exports = router;
