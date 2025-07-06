c803bf2dbcbba12c0f39b753963e1e243fb7ee12
linolazarous <linolaku560@gmail.com>
2025-07-06 18:59:39 +0200

    Update microLearningRoutes.js

--- backend/src/routes/microLearningRoutes.js
+++ backend/src/routes/microLearningRoutes.js
@@ -1,10 +1,24 @@
-const express = require("express");
-const { createLesson, getAllLessons } = require("../controllers/microLearningController");
-const authMiddleware = require("../middleware/authMiddleware");
-
+const express = require('express');
 const router = express.Router();
+const { createLesson, getAllLessons } = require('../controllers/microLearningController');
+const authMiddleware = require('../middleware/authMiddleware');
+const { validateMicroLesson } = require('../middleware/validationMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+const rateLimit = require('../middleware/rateLimitMiddleware');
 
-router.post("/", authMiddleware, createLesson);
-router.get("/", getAllLessons);
+router.post(
+  '/',
+  authMiddleware,
+  roleMiddleware(['instructor', 'admin']),
+  validateMicroLesson,
+  rateLimit('20req/day'),
+  createLesson
+);
+
+router.get(
+  '/',
+  rateLimit('1000req/hour'),
+  getAllLessons
+);
 
 module.exports = router;
