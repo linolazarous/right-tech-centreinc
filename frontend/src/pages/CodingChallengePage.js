5f83b873c4d138dee7d8a1b1cd14e154ab1605b8
linolazarous <linolaku560@gmail.com>
2025-07-08 16:37:22 +0200

    Update CodingChallengePage.js

--- frontend/src/pages/CodingChallengePage.js
+++ frontend/src/pages/CodingChallengePage.js
@@ -1,12 +1,26 @@
 import React from 'react';
-  import CodingChallenge from '../components/CodingChallenge';
+import CodingChallenge from '../components/CodingChallenge';
+import PageLayout from '../layouts/PageLayout';
+import { usePageTracking } from '../hooks/usePageTracking';
+import ErrorBoundary from '../components/ErrorBoundary';
+import { useAuth } from '../contexts/AuthContext';
 
-  const CodingChallengePage = () => {
-      return (
-          <div>
-              <CodingChallenge />
-          </div>
-      );
-  };
+const CodingChallengePage = () => {
+  const { currentUser } = useAuth();
+  usePageTracking();
 
-  export default CodingChallengePage;
+  return (
+    <PageLayout 
+      title="Coding Challenges"
+      protectedRoute
+      seoTitle="Coding Challenges | Right Tech Centre"
+      seoDescription="Test and improve your coding skills with our interactive challenges"
+    >
+      <ErrorBoundary>
+        <CodingChallenge userId={currentUser?.id} />
+      </ErrorBoundary>
+    </PageLayout>
+  );
+};
+
+export default React.memo(CodingChallengePage);
