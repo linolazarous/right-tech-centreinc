4cd20d0e2b731429d2df187f3d6bfb0d0bef6699
linolazarous <linolaku560@gmail.com>
2025-09-06 23:06:41 +0200

    Update vrCareerFairService.js

--- backend/src/services/vrCareerFairService.js
+++ backend/src/services/vrCareerFairService.js
@@ -1,5 +1,5 @@
-const axios = require('axios');
-const logger = require('../utils/logger');
+import axios from 'axios';
+import logger from '../utils/logger.js';
 
 class VRCareerFairService {
   static async createVREvent(eventName, eventDetails) {
@@ -44,4 +44,4 @@ class VRCareerFairService {
   }
 }
 
-module.exports = VRCareerFairService;
+export default VRCareerFairService;
