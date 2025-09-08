935a16da9a88864120c668ea587f89cde944a99e
linolazarous <linolaku560@gmail.com>
2025-09-08 20:05:28 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -7,53 +8,41 @@ import LiveClass from './pages/LiveClass';
 import Forum from './pages/Forum';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
-import ContactPage from './pages/ContactPage'; // New contact page
-import FormSuccess from './pages/FormSuccess'; // New success page
+import ContactPage from './pages/ContactPage';
+import FormSuccess from './pages/FormSuccess';
+import APITest from './components/APITest'; // Import the API test component
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { Toaster } from 'react-hot-toast'; // For notification messages
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
+    
+      
+
+        
         
         {/* Notification system */}
-        <Toaster
-          position="top-center"
-          toastOptions={{
-            duration: 5000,
-            style: {
-              background: '#363636',
-              color: '#fff',
-            },
-          }}
-        />
+        
 
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/live-class" element={<LiveClass />} />
-            <Route path="/forum" element={<Forum />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-of-service" element={<TermsOfService />} />
-            
-            {/* New routes for contact form flow */}
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/form-success" element={<FormSuccess />} />
-            
-            {/* 404 Page - Add this if you want */}
-            <Route path="*" element={<div>404 Not Found</div>} />
-          </Routes>
-        </main>
+        
 
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+          
+            } />
+            } />
+            } />
+            } />
+            } />
+            } />
+            } />
+            } />
+            } />
+            } /> {/* API test route */}
+            404 Not Found
+} />
+          
+        
 
-export default App;
+        
+
+      
