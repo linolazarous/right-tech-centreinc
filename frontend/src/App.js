52c81a789adca2f3daaa963c8b8f05bc76719db5
linolazarous <linolaku560@gmail.com>
2025-09-08 22:15:35 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -10,39 +9,49 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
 import ContactPage from './pages/ContactPage';
 import FormSuccess from './pages/FormSuccess';
-import APITest from './components/APITest'; // Import the API test component
+import APITest from './components/APITest';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { Toaster } from 'react-hot-toast';
 
+// A simple component for your 404 page
+function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>404 - Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+}
+
 function App() {
   return (
-    
+    <Router>
+      {/* Toaster provides notifications throughout the app */}
+      <Toaster position="top-center" reverseOrder={false} />
       
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/live-class" element={<LiveClass />} />
+            <Route path="/forum" element={<Forum />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/success" element={<FormSuccess />} />
+            <Route path="/api-test" element={<APITest />} />
+            {/* Catch-all route for 404 Not Found pages */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
-        
-        
-        {/* Notification system */}
-        
-
-        
-
-          
-            } />
-            } />
-            } />
-            } />
-            } />
-            } />
-            } />
-            } />
-            } />
-            } /> {/* API test route */}
-            404 Not Found
-} />
-          
-        
-
-        
-
-      
+export default App;
