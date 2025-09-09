e887ebf007bc02c1fa63fc3c730d7243d009b012
linolazarous <linolaku560@gmail.com>
2025-09-09 21:40:56 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+
+// IMPORTANT: Replace this with your actual reCAPTCHA v3 Site Key
+const RECAPTCHA_SITE_KEY = 'YOUR_RECAPTCHA_V3_SITE_KEY';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_SITE_KEY}>
+      <App />
+    </GoogleReCaptchaProvider>
+  </React.StrictMode>
+);
+
+
+
+old one merge it to the above file
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
@@ -11,4 +31,5 @@ ReactDOM.render(
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
+
 );
