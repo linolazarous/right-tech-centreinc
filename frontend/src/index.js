1fe4a710fc51d84e23295db4849a730c9a3aedf6
linolazarous <linolaku560@gmail.com>
2025-09-09 21:50:55 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,35 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import App from './App';
+import './assets/styles/global.css';
 
 // IMPORTANT: Replace this with your actual reCAPTCHA v3 Site Key
-const RECAPTCHA_SITE_KEY = 'YOUR_RECAPTCHA_V3_SITE_KEY';
+const RECAPTCHA_SITE_KEY = process.env.REACT_APP_RECAPTCHA_SITE_KEY || 'YOUR_RECAPTCHA_V3_SITE_KEY';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_SITE_KEY}>
-      <App />
-    </GoogleReCaptchaProvider>
+    <BrowserRouter>
+      <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_SITE_KEY}>
+        <App />
+      </GoogleReCaptchaProvider>
+    </BrowserRouter>
   </React.StrictMode>
-);
-
-
-
-old one merge it to the above file
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './assets/styles/global.css';
-
-ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
-
 );
