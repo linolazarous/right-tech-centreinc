c337f87a7f312a70d7d7221875d2bc2764f56e68
linolazarous <linolaku560@gmail.com>
2025-09-10 14:32:44 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CoursePage from './pages/CoursePage';
 import ProfilePage from './pages/ProfilePage';
@@ -13,6 +13,8 @@ import APITest from './components/APITest';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { Toaster } from 'react-hot-toast';
+import { setUserContext, clearUserContext } from './utils/sentry';
+import SentryErrorBoundary from './components/SentryErrorBoundary';
 
 // A simple component for your 404 page
 function NotFound() {
@@ -24,35 +26,57 @@ function NotFound() {
   );
 }
 
+function AppContent() {
+  const location = useLocation();
+
+  // Set user context when user logs in
+  React.useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('userData'));
+    if (user) {
+      setUserContext({
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      });
+    } else {
+      clearUserContext();
+    }
+  }, [location]);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/courses" element={<CoursePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/live-class" element={<LiveClass />} />
+          <Route path="/forum" element={<ForumPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/success" element={<FormSuccess />} />
+          <Route path="/api-test" element={<APITest />} />
+          {/* Catch-all route for 404 Not Found pages */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      {/* Toaster provides notifications throughout the app */}
-      <Toaster position="top-center" reverseOrder={false} />
-      
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/courses" element={<CoursePage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/live-class" element={<LiveClass />} />
-            <Route path="/forum" element={<ForumPage />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-of-service" element={<TermsOfService />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/success" element={<FormSuccess />} />
-            <Route path="/api-test" element={<APITest />} />
-            {/* Catch-all route for 404 Not Found pages */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <SentryErrorBoundary>
+        {/* Toaster provides notifications throughout the app */}
+        <Toaster position="top-center" reverseOrder={false} />
+        <AppContent />
+      </SentryErrorBoundary>
     </Router>
   );
 }
 
 export default App;
-
