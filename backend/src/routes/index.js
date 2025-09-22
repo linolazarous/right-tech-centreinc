4479730057123a5e79aa421c909744f589ccaed4
linolazarous <linolaku560@gmail.com>
2025-09-22 15:43:30 +0200

    Update index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,14 +2,12 @@ import authRoutes from './auth.js';
 import userRoutes from './users.js';
 import adminRoutes from './admin.js';
 
-// Export as an object for easy importing
 export {
   authRoutes,
   userRoutes,
   adminRoutes
 };
 
-// Default export for easy use in server.js
 export default {
   authRoutes,
   userRoutes,
