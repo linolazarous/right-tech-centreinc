8d1680f9301cab0f97613d1b99b3bf9e54052b69
linolazarous <linolaku560@gmail.com>
2025-10-01 22:28:17 +0200

    Update helpers.js

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -1,10 +1,15 @@
-// utils/helpers.js
 import mongoose from 'mongoose';
 
 export const isValidObjectId = (id) => {
   return mongoose.Types.ObjectId.isValid(id);
 };
 
-export default {
-  isValidObjectId
+export const sanitizeUser = (user) => {
+  const userObj = user.toObject ? user.toObject() : user;
+  const { password, twoFASecret, verificationToken, passwordResetToken, ...sanitized } = userObj;
+  return sanitized;
+};
+
+export const generateRandomString = (length = 10) => {
+  return Math.random().toString(36).substring(2, 2 + length);
 };
